3f2638a948ea166673c0da85a03d1b2c9bdb5a34
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-23 00:30:18 +0300

    edited:front-page,view,edit pages

--- wp-content/plugins/Plana/templates/edit-event.php
+++ wp-content/plugins/Plana/templates/edit-event.php
@@ -1,14 +1,12 @@
 <?php
 $event_id = $_GET['event_id'];
 
-// var_dump($ticket_id);
-
 global $wpdb;
 $table = $wpdb->prefix . 'events';
 
 $data = $wpdb->get_results("SELECT * FROM $table WHERE event_id = '$event_id'");
 
-var_dump($data);
+$data = $data[0]; 
 
 ?>
 
@@ -21,49 +19,48 @@ var_dump($data);
         <h1 style="text-align:center;">Edit An Event</h1>
         <div>
             <label>Event Name:</label><br>
-            <input type="text" style="width:100%;" placeholder="Enter event name" value=""  name="edt_event_name" required>
+            <input type="text" style="width:100%;" placeholder="Enter event name" value="<?php echo $data->event_name; ?>"  >
         </div>
 
         <div>
             <label>Event Poster:</label><br>
-            <input type="text" style="width:100%;" placeholder="Enter event name" value=""  name="edt_event_poster" required>
+            <input type="text" style="width:100%;" placeholder="Enter event name" value="<?php echo $data->event_poster; ?>"  name="edt_event_poster" required>
         </div>
 
         <label>Event Category:</label><br>
-        <select class="form-select-lg" aria-label="Default select example" value=""  name="edt_event_category" style="margin-top:5px;width:100%;">
-
-            <option selected style="width:100%;" name="event_category">choose category</option>
-            <option >Sport Events</option>
-            <option >Music Concerts</option>
-            <option >Modelling</option>
-            <option >Trips & Adventure</option>
+        <select class="form-select-lg" aria-label="Default select example" style="margin-top:5px;width:100%;" name="edt_event_category">
+            <option selected disabled>Choose category</option>
+            <option <?php if ($data->event_category == "Sport Events") echo "selected"; ?>>Sport Events</option>
+            <option <?php if ($data->event_category == "Music Concerts") echo "selected"; ?>>Music Concerts</option>
+            <option <?php if ($data->event_category == "Modelling") echo "selected"; ?>>Modelling</option>
+            <option <?php if ($data->event_category == "Trips & Adventure") echo "selected"; ?>>Trips & Adventure</option>
         </select>
 
         <div>
             <label>Event Date:</label><br>
-            <input type="date" style="width:100%;" placeholder="Event Date" value=""  name="edt_event_date" required>
+            <input type="date" style="width:100%;" placeholder="Event Date" value="<?php echo $data->event_date; ?>"  name="edt_event_date" required>
         </div>
 
         <div>
             <label>Event Time:</label><br>
-            <input type="text" style="width:100%;" placeholder="Event time" value="" name="edt_event_time" required>
+            <input type="text" style="width:100%;" placeholder="Event time" value="<?php echo $data->event_time; ?>" name="edt_event_time" required>
         </div>
 
         <div>
             <label>Event Location:</label><br>
-            <input type="text" style="width:100%;" placeholder="Enter event location" value=""  name="edt_event_location" required>
+            <input type="text" style="width:100%;" placeholder="Enter event location" value="<?php echo $data->event_location; ?>"  name="edt_event_location" required>
         </div>
 
         <div class="form-outline">
             <label>Event Description:</label><br>
-            <textarea class="form-control" rows="4" type="text" style="width:100%;" value=""  placeholder="Enter event details"
+            <textarea class="form-control" rows="4" type="text" style="width:100%;" value="<?php echo $data->event_description; ?>"  placeholder="Enter event details"
                 name="edt_event_desc" required></textarea>
         </div>
 
         <div>
             <label>Organizer Information </label><br>
             <input type="text" style="width:100%;" placeholder="Enter event organizer's information"
-            value=""  name="edt_organizer_info" required><br>
+            value="<?php echo $data->organizer_info; ?>"  name="edt_organizer_info" required><br>
 
         </div>
 
--- wp-content/themes/eventmanagement/front-page.php
+++ wp-content/themes/eventmanagement/front-page.php
@@ -24,15 +24,15 @@ $carousel3 = json_decode(json_encode($trips), true);
     <div id="carouselExampleAutoplaying" class="carousel slide mb-3 "
         style="width:100%;height:28rem;box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;"
         data-bs-ride="carousel">
-        <div class="carousel-inner h-100" style="">
+        <div class="carousel-inner h-100" >
             <div class="carousel-item active">
-                <img src="<?php echo $event_poster = $carousel[0]['event_poster']; ?>" class="d-block w-100" alt="...">
+                <img src="<?php echo $carousel[0]['event_poster']; ?>" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="<?php echo $event_poster = $carousel1[1]['event_poster']; ?>" class="d-block w-100" alt="...">
+                <img src="<?php echo $carousel1[1]['event_poster']; ?>" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="<?php echo $event_poster = $carousel2[2]['event_poster']; ?>" class="d-block w-100" alt="...">
+                <img src="<?php echo $carousel2[2]['event_poster']; ?>" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
@@ -51,8 +51,8 @@ $carousel3 = json_decode(json_encode($trips), true);
     <div
         style="background-color:#ECF0F1 ;width:80%;height:auto;margin: 0 10%;border-radius:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
         <div class="Sport">
-            <div class="" style="color:black;padding:3px;font-weight:500;">
-                <p>Sport Events</p>
+            <div class="" style="color:black;padding:3px;font-weight:500;margin-left:10px;">
+                <p>Sports</p>
             </div>
             <div style="display:flex;justify-content:center;flex-direction:row; width:100%;margin-bottom:10px;">
 
@@ -60,16 +60,22 @@ $carousel3 = json_decode(json_encode($trips), true);
                 for ($i = 0; $i < min(count($carousel1), 4); $i++) {
                     $sport = $carousel1[$i];
                     ?>
-
-                    <img src="<?php echo $event_poster = $carousel1[$i]['event_poster'] ?>" alt=""
-                        style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+                    <form action="event-details.php" method="get" class="hover-zoom">
+                        <input type="hidden" name="event_id" value="<?php echo $sport['event_poster']; ?>">
+                        <button type="submit" style="background:none;border:none;padding:0;">
+                            <img src="<?php echo esc_url($sport['event_poster']); ?>"
+                                style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+                        </button>
+                        <!-- <p style="font-size:10px;"></p><?php //echo ($sport['event_name']); ?></p> -->
+                    </form>
 
                 <?php } ?>
+
             </div>
         </div>
 
         <div class="Sport">
-            <div class="" style="color:black;padding:3px;font-weight:500;">
+            <div class="" style="color:black;padding:3px;font-weight:500;margin-left:10px;">
                 <p>Music Concerts</p>
             </div>
             <div style="display:flex;justify-content:center;flex-direction:row; width:100%;margin-bottom:10px;">
@@ -77,32 +83,38 @@ $carousel3 = json_decode(json_encode($trips), true);
                 <?php
                 for ($i = 0; $i < min(count($carousel), 4); $i++) {
                     $music = $carousel[$i];
+                  
                     ?>
 
-                    <img src="<?php echo $event_poster = $carousel[$i]['event_poster'] ?>" alt=""
-                        style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
-
+                    <form action="event-details.php" method="get">
+                        <input type="hidden" name="event_id" value="<?php echo $music['event_poster']; ?>">
+                        <button type="submit" style="background:none;border:none;padding:0;">
+                            <img src="<?php echo esc_url($music['event_poster']); ?>"
+                                style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+                        </button>
+                    </form>
                 <?php } ?>
             </div>
         </div>
 
-       
+
         <div class="Sport">
-            <div class="" style="color:black;padding:3px;font-weight:500;">
+            <div class="" style="color:black;padding:3px;font-weight:500;margin-left:10px;">
                 <p>Fashion and Modelling</p>
             </div>
             <div style="display:flex;justify-content:center;flex-direction:row; width:100%;margin-bottom:10px;">
 
                 <?php
                 for ($i = 0; $i < min(count($carousel2), 4); $i++) {
-                    $music = $carousel2[$i];
+                    $model = $carousel2[$i];
                     ?>
-                        <div>
-                    <img src="<?php echo $event_poster = $carousel2[$i]['event_poster'] ?>" alt=""
-                        style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
-
-                        <p><?php echo $event_poster = $carousel2[$i]['event_name'] ?></p>
-                        </div>
+                    <form action="event-details.php" method="get">
+                        <input type="hidden" name="event_id" value="<?php echo $model['event_poster']; ?>">
+                        <button type="submit" style="background:none;border:none;padding:0;">
+                            <img src="<?php echo esc_url($model['event_poster']); ?>"
+                                style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+                        </button>
+                    </form>
 
                 <?php } ?>
 
@@ -110,19 +122,23 @@ $carousel3 = json_decode(json_encode($trips), true);
         </div>
 
         <div class="Sport">
-            <div class="" style="color:black;padding:3px;font-weight:500;">
+            <div class="" style="color:black;padding:3px;font-weight:500;margin-left:10px;">
                 <p>Trips & Adventures</p>
             </div>
             <div style="display:flex;justify-content:center;flex-direction:row; width:100%;margin-bottom:10px;">
 
                 <?php
                 for ($i = 0; $i < min(count($carousel3), 4); $i++) {
-                    $music = $carousel3[$i];
+                    $trip = $carousel3[$i];
                     ?>
 
-                    <img src="<?php echo $event_poster = $carousel3[$i]['event_poster'] ?>" alt=""
-                        style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
-
+                    <form action="event-details.php" method="get">
+                        <input type="hidden" name="event_id" value="<?php echo $trip['event_poster']; ?>">
+                        <button type="submit" style="background:none;border:none;padding:0;">
+                            <img src="<?php echo esc_url($trip['event_poster']); ?>"
+                                style="width:18vw;height:45vh;border-radius:5px;margin:10px;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+                        </button>
+                    </form>
                 <?php } ?>
             </div>
         </div>
